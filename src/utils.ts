efb771028ddada78d018a6742c85b38f75dfdf82
nrdsstuff <nrdfoss@gmail.com>
2025-02-16 19:47:22 +0100

    indents time

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
 function randomID() {
-  const chars = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-  let result = "";
-  for (let i = 0; i < 24; i++) {
-    result += chars[Math.round(Math.random() * 100 % 35)];
-  }
-  return result;
+    const chars = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    let result = "";
+    for (let i = 0; i < 24; i++) {
+        result += chars[Math.round(Math.random() * 100 % 35)];
+    }
+    return result;
 }
 
 export {randomID};
