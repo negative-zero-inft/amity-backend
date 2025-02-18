82c7d83ea854303991a4c263f91aa5a0df695f38
nrdsstuff <nrdfoss@gmail.com>
2025-02-18 20:26:46 +0100

    no pfps by default

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const app = new Elysia()
             .use(discord)
             .use(osu)
     )
-    .post('/register', async ({ set, body: { password, tag, name, avatar } }) => {
+    .post('/register', async ({ set, body: { password, tag, name } }) => {
         const randomid = randomID();
         // await sql`INSERT INTO amity_id (id, server) VALUES (${randomid}, ${server})`;
         // await sql`INSERT INTO users (id, tag, name, password, avatar) VALUES 
@@ -44,7 +44,7 @@ const app = new Elysia()
             id: amityId,
             tag: tag,
             name: name,
-            avatar: avatar,
+            avatar: "https://media.discordapp.net/attachments/1339688907679465472/1341490498824835163/appicon.png?ex=67b62fe2&is=67b4de62&hm=4401408bdeb715575d0df621070cba094995c3c40ddf1b98bf0cecec1c656588&=&format=webp&quality=lossless&width=672&height=672",
             password: await Bun.password.hash(password)
         });
         await user.save();
