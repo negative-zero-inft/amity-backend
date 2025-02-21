3023d5c78c3cbc8ebee8be0f6ee0807ce1dc87ad
meowstercatel <66054649+meowstercatel@users.noreply.github.com>
2025-02-21 14:37:07 +0100

    sending dms

--- src/dm.ts
+++ src/dm.ts
@@ -6,6 +6,7 @@ import { User } from './schema/user';
 import { AmityId } from './schema/amityId';
 import { Channel } from './schema/channel';
 import { Chat } from './schema/chat';
+import { Message } from './schema/message';
 
 export const dm = new Elysia()
     .use(
@@ -39,6 +40,38 @@ export const dm = new Elysia()
         }, {
             body: t.Object({
                 user_id: t.String(), //amity id
+            })
+        })
+        .post("/:id/send", async ({ jwt, set, query, body, params: {id}}) => {
+                const profile = await jwt.verify(query.token)
+                if (!profile) {
+                    set.status = 401;
+                    return 'Unauthorized';
+                }
+                const chat = await Chat.findOne({"id.id": id});
+                const amityId = new AmityId({ id: profile.id, server: process.env.SERVER_URL })
+                const message = new Message({
+                    date: body.date,
+                    author_id: amityId,
+                    encrypted: body.encrypted,
+                    content: body.content,
+                    contents: body.contents
+                });
+                await message.save();
+                chat?.messages.push(message);
+                await chat?.save();
+        }, {
+            body: t.Object({
+                date: t.Date(),
+                encrypted: t.Boolean(),
+                content: t.Optional(t.String()),
+                contents: t.Optional(t.Array(t.Object({
+                    for: t.Object({
+                        id: t.String(),
+                        server: t.String()
+                    }),
+                    content: t.String()
+                })))
             })
         })
         .get("/chats", async ({ query, jwt, set }) => {
