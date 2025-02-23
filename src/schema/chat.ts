e591515a92a27c1378f617d3528ff24507440b92
meowstercatel <66054649+meowstercatel@users.noreply.github.com>
2025-02-23 22:44:18 +0100

    chat members

--- src/schema/chat.ts
+++ src/schema/chat.ts
@@ -3,6 +3,7 @@ import { amityIdSchema } from './amityId';
 
 export const chatSchema = new mongoose.Schema({
     id: amityIdSchema,
+    members: [amityIdSchema],
     pinned: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
 });
